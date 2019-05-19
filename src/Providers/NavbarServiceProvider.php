e800cf5b0e4738371904288b81de096364d3fdf5
Cucurrucho <adam.bar91@gmail.com>
2019-05-19 14:02:23 -0700

    elcoop navbar package

---
+++ src/Providers/NavbarServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ElCoop\Navbar\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NavbarServiceProvider extends ServiceProvider {
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+
+	}
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+
+		$this->loadViewsFrom(__DIR__ . '/../Views', 'elcoop:navbar');
+		$this->publishes([__DIR__ . '/../../resources/js' => resource_path('js/vendor/elcoop/navbar')]);
+	}
+}
